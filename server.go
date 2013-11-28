4d58e7b23292c31efc8ec8241ee8c3dc11ff30b8
Ramkumar Ramachandra <artagnon@gmail.com>
2013-11-28 15:03:55 +0530

    server: render a dummy json string into the template

--- server.go
+++ server.go
@@ -2,38 +2,33 @@ package main
 
 import (
 	"html/template"
-	"io/ioutil"
 	"net/http"
 	"github.com/gorilla/mux"
+	"encoding/json"
 )
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := mux.Vars(r)["title"]
 	p, _ := loadPage(title)
-	t, err := template.ParseFiles("/tmp/view.html")
+	t, err := template.ParseFiles("view.html")
 	if err != nil {
-		panic("Cannot open /tmp/view.html" + err.Error())
+		panic("Cannot open view.html" + err.Error())
 	}
 	t.Execute(w, p)
 }
 
 type Page struct {
 	Title string
-	Body []byte
+	Body string
 }
 
-func (p *Page) save() error {
-	filename := "/tmp/" + p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+type Message struct {
+	Subject string
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := "/tmp/" + title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
+	bytes, _ := json.Marshal(Message{"foom"})
+	return &Page{title, string(bytes)}, nil
 }
 
 func main() {
