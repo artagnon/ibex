c8fa4e0ffaa977269fbf331288d527ca730b5f13
Ramkumar Ramachandra <artagnon@gmail.com>
2013-11-30 23:40:29 +0530

    httpd: remove viewHandler

--- httpd.go
+++ httpd.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 	"github.com/gorilla/mux"
-	"encoding/json"
 	"log"
 )
 
-type Page struct {
-	Title string
-	Body string
-}
-
-type Message struct {
-	Subject string
-}
+var c *imap.Client
 
 func requestLogger(handler http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -24,24 +15,8 @@ func requestLogger(handler http.Handler) http.Handler {
     })
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := mux.Vars(r)["title"]
-	p, _ := loadPage(title)
-	t, err := template.ParseFiles("www/templates/view.html")
-	if err != nil {
-		panic("Cannot open view.html" + err.Error())
-	}
-	t.Execute(w, p)
-}
-
-func loadPage(title string) (*Page, error) {
-	bytes, _ := json.Marshal(Message{"foom"})
-	return &Page{title, string(bytes)}, nil
-}
-
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/view/{title}", viewHandler)
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("www")))
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", requestLogger(http.DefaultServeMux))
