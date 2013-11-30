4ef11869ba4014b01aa2dd8c9fa3569e9ede3990
Ramkumar Ramachandra <artagnon@gmail.com>
2013-11-30 19:58:36 +0530

    www/mailbox: display list of emails from json dump

--- www/app.js
+++ www/app.js
@@ -1,3 +1,22 @@
-function Mailbox ($scope) {
+function Mailbox ($scope, $http) {
     $scope.mailboxes = ["Inbox", "All Mail"];
+    $scope.format_subject = function (mail) {
+	return mail["Subject"].replace(/^(Re:|Fwd:)+ /, "");
+    };
+    $scope.format_authors = function (conversation) {
+	var authors = _.map(conversation, function (mail) {
+	    var from = mail["From"]["Name"]
+	    if (!from) {
+		return mail["From"]["Address"].split("@")[0];
+	    } else if (from.indexOf(", ") !== -1) {
+		return from.split(", ")[1];
+	    } else {
+		return from.split(" ") [0];
+	    }
+	});
+	return _.uniq(authors).join(", ");
+    };
+    $http.get('mails.json').success(function(data) {
+	$scope.conversations = data;
+    });
 };
